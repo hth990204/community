2259d7d3b066b8d869239d5d18f438c1ac20735d
hth990204 <875177543@qq.com>
2022-07-06 10:14:21 +0800

    包含注册、发送验证邮件与登录验证码

---
+++ src/test/java/com/nowcoder/community/MailTests.java
@@ -0,0 +1,42 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.mail.MessagingException;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail() throws MessagingException {
+        mailClient.sendMail("875177543@qq.com", "TEST", "Welcome");
+    }
+
+    @Test
+    public void textHtmlMail() throws MessagingException {
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+        System.out.println(content);
+
+        mailClient.sendMail("875177543@qq.com", "HTML", content);
+    }
+
+}
